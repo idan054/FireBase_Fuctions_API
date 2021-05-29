e011d4b7a1c66f7dcb1521310e18c07d407b6517
Biton's Mac <idanbit80@gmail.com>
2021-05-29 13:13:38 +0300

    V2.0 Working Post & Get Request with same url - Post params based Json body - Get param based https://url?query=yes
    V1.0 Working API Post request for database
    push API updates by:
    $ firebase deploy --only functions
    
    More useful commands:
    $ firebase init (Connect to firebase project)
    $ firebase login
    $ firebase logout
    
    To use:
    - Get link from Functions in firebase
    - Make sure to use post method
    - Fill "collectionPath" like  "collectionName/DocRef/InnerCollection"
    - Add any other fields as you wish to (On raw - json)
    
    Video guide I used (Medium article on desc)
    https://www.youtube.com/watch?v=iIVlRZIo2-c&t=591s
    
    Common issue known as “eslint”
    ========================
    Part from error:
    20 error code ELIFECYCLE 21 error errno 1 22 error functions@ lint: `eslint .` 22 error Exit status 1 23 error Failed at the functions@ lint script.
    Solution:
    The linter package isn't installed, if you need to deploy without run linter you can remove the those lines from firbase.json file:
    
    "predeploy": [
          "npm --prefix \"$RESOURCE_DIR\" run lint"
        ],
    
    # Credit @Mohammad Desouky

--- functions/index.js
+++ functions/index.js
@@ -12,14 +12,46 @@ admin.initializeApp();
 
 const app = express();
 
+// Firebase functions not have POST GET (?) , so we make it.
+app.get('/', async (req,
+               res) => {
+  // const {collection} = req.query;
+
+  //etc. example.com/user/000000?sex=female
+  const query = req.query;// query = {sex:"female"}
+  const collectionPath = query["collectionPath"] // Collection/DocReference/InnerCollection
+
+  const params = req.params; //params = {id:"000000"}
+  // const collectionPath = params["collectionPath"] // Collection/DocReference/InnerCollection
+
+  // const reqBody = req.body
+  // const collectionPath = reqBody["collectionPath"] // Collection/DocReference/InnerCollection
+
+  // My Get req.url: https://us-central1-bubbleflow-mitmit.cloudfunctions.net/database_GetPost?collectionPath=users
+
+  const snapshot = await admin.firestore().collection(collectionPath).get();
+
+  let users = [];
+  snapshot.forEach(doc => {
+    let id = doc.id;
+    let createTime = doc.createTime;
+    let data = doc.data();
+
+    users.push({id, ...data, createTime})
+  });
+
+  res.status(201).send(JSON.stringify(users));
+
+})
+
 app.post("/", async (req, res) => {
-  const user = req.body
-  const collectionPath = user["collectionPath"] // Collection/DocReference/InnerCollection
-  await admin.firestore().collection(collectionPath).add(user)
+  const reqBody = req.body
+  const collectionPath = reqBody["collectionPath"] // Collection/DocReference/InnerCollection
+  await admin.firestore().collection(collectionPath).add(reqBody)
 
   res.status(201).send();
 })
-exports.database_post = functions.https.onRequest(app);
+exports.database_GetPost = functions.https.onRequest(app);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
